a5faee6dd7efe51e7a81f84f5a0a11c95e4d552f
Cryotechnic <cryogen65@gmail.com>
2020-10-08 15:51:04 -0400

    New code for clear understanding & fixes to file structure

---
+++ Code Snippets/Oct 5/file_reader.py
@@ -0,0 +1,17 @@
+# This file is designed to smoothly access and read files, line by line
+# using the pandas package. Each line is separated by the /n character sequence
+# while outputing in DataFrames, then converting to dictionaries
+# Programmed by Ron Friedman, 1926133
+
+
+
+# Importing libraries
+import pandas as pd
+
+# Reading file and printing first 100 rows
+file = pd.read_csv("titanic.csv")
+print(file.head(100))
+
+# Converting to dictionary
+file.to_dict()
+print(file)
