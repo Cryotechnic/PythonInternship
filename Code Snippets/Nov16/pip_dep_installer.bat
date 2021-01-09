45fc0d501edfabc80ecff43067b4d10cc81208eb
Cry0 <cryogen65@gmail.com>
2021-01-09 15:11:56 -0500

    Dependencies & pip upgrade

--- Code Snippets/Nov16/pip_dep_installer.bat
+++ Code Snippets/Nov16/pip_dep_installer.bat
@@ -1,4 +1,5 @@
 @echo off
+python3 -m pip install --upgrade pip
 pip3 install matplotlib
 pip3 install tensorflow
 pip3 install ipykernel
@@ -6,4 +7,4 @@ pip3 install keras
 pip3 install pandas
 pip3 install numpy
 pip3 install opencv-python 
-python3 -m pip install --upgrade pip
+pip3 install pysimplegui
